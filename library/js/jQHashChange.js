436bcdfefb2779ae58934b39fccc297d273c0e1f
Arabirim <aycan.bulbul@arabirim.com.tr>
2014-05-30 16:50:42 +0300

    Create jQHashChange.js

---
+++ library/js/jQHashChange.js
@@ -0,0 +1,23 @@
+window.previousHash = '';
+
+function jQHashChange()
+{
+    var newHash = '';
+    var url = window.location.href.split('#')[1];
+
+    var zaman = setInterval
+    (
+        function ()
+        {
+            newHash = (window.location.href.split('#')[1] || '');
+
+            if( (previousHash !== newHash) && (newHash.length > 1) )
+            {
+                clearInterval(zaman);
+                previousHash = newHash;
+                bodyAppend( newHash );
+                jQHashChange();
+            } 
+        },100
+    );
+}
